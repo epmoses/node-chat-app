1d0b1559df5465a70555cd1d738b51dc3bf070cf
Emily Moses <emilypatriciamoses@gmail.com>
2018-12-13 15:14:00 -0600

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
-let app = express();
 const path = require('path');
-const port = process.env.PORT || 3000
+const express = require('express');
+const http = require('http'); // built in node module
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+
+// Socket.io setup
+let io = socketIO(server);
+
 const publicPath = path.join(__dirname, '../public');
 
+// Express Middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Socket.io setup
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected from server')
+    })
+});
+
+// Port
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
