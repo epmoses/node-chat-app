9a4268bce2c4372efb1ee28388836b2b382831bf
Emily Moses <emilypatriciamoses@gmail.com>
2019-01-02 15:51:10 -0600

    add mustachejs front end

--- public/js/index.js
+++ public/js/index.js
@@ -11,23 +11,26 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     let formattedTime = moment(message.createdAt).format('h:mm a');
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    console.log('newMessage', message);
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
-    formattedTime = moment(message.createdAt).format('h:mm a')
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>');
-
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    let formattedTime = moment(message.createdAt).format('h:mm a');
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    
+    jQuery('#messages').append(html);
 })
 
 // Form
